f92be0cda58054dfd2d7c90d554efda9acce9d24
Vikram Chatterjee <strycora@gmail.com>
2021-01-09 16:23:31 -0700

    added postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20201229234939) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dragons", force: :cascade do |t|
     t.string   "name"
     t.string   "color"
